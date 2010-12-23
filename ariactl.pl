435145e7dfb8e36200c97912a51e2e80bfb59888
Frédéric Perrin <frederic.perrin@resel.fr>
2010-12-23 23:09:44 +0000

    Fix the indentation (no functional changes)
    git-svn-id: https://svn.fperrin.net/cmd@515 c9a10d8c-32d9-de11-9687-001bfc191731

--- ariactl.pl
+++ ariactl.pl
@@ -7,13 +7,13 @@ use warnings;
 use CGI qw/:standard *table *Tr *td/;
 #use CGI::Pretty qw/ :standard *table *Tr *td/;
 use CGI::Carp qw/fatalsToBrowser warningsToBrowser/;
+use File::Basename;
+use Number::Format qw/:subs/;
+use Tie::IxHash;
 
 use Net::INET6Glue::INET_is_INET6;
 use Frontier::Client;
-use Tie::IxHash;
 
-use Number::Format qw/:subs/;
-use File::Basename;
 use Data::Dumper;
 
 # There are also log and log-level, not sure how useful these are in a Web
@@ -21,55 +21,54 @@ use Data::Dumper;
 # this list. No: getGlobalOption returns way too many options (including many
 # that can't be modified from the XML-RPC interface).
 our %ariaopts = ("max-concurrent-downloads" => "Max simultaneous downloads",
-	        "max-overall-download-limit" => "Overall d/l bandwidth limit",
-	        "max-overall-upload-limit" => "Overall u/l bandwidth limit");
+                 "max-overall-download-limit" => "Overall d/l bandwidth limit",
+                 "max-overall-upload-limit" => "Overall u/l bandwidth limit");
 
 our %ariadlopts = ("max-download-limit" => "D/L bandwidth limit",
-		  "max-upload-limit" => "U/L bandwidth limit",
-		  "bt-max-peers" => "Max nb. of peers",
-		  "bt-request-peer-speed-limit" => "Lower peer speed limit");
+                   "max-upload-limit" => "U/L bandwidth limit",
+                   "bt-max-peers" => "Max nb. of peers",
+                   "bt-request-peer-speed-limit" => "Lower peer speed limit");
 
 sub show_dl {
     my $dls = shift;
     if (scalar @$dls) {
-	my $odd = 1;
-	print start_table();
-	print Tr(th(["Filename", "Status", "Progress"]));
-	foreach my $dl (@$dls) {
-	    print start_Tr({-class => ($odd ? "oddrow" : "evenrow")});
-	    $odd = !$odd;
+        my $odd = 1;
+        print start_table();
+        print Tr(th(["Filename", "Status", "Progress"]));
+        foreach my $dl (@$dls) {
+            print start_Tr({-class => ($odd ? "oddrow" : "evenrow")});
+            $odd = !$odd;
 
-	    my ($basedir) = ($dl->{files}[0]{path} =~ m#^(.*)/#);
-	    print td(dl(dt({ -onClick => "toggle(".$dl->{gid}.")" }, $basedir),
-			map({ +dd({-id => $dl->{gid}}, $_->{path}) }
-			    @{$dl->{files}})));
-	    my $progress;
-	    if ($dl->{totalLength}) {
-		$progress = sprintf(
-		    "%.2f%% (%s/%s)",
-		    100*$dl->{completedLength} / $dl->{totalLength},
-		    format_bytes($dl->{completedLength}),
-		    format_bytes($dl->{totalLength}));
-	    } else {
-		$progress = "n/a";
-	    }
-	    print td(p($dl->{status}),
-		     start_form(),
-		     popup_menu(-name => "dloptname", -values => [keys %ariadlopts],
-				-labels => \%ariadlopts),
-		     textfield(-name => "dloptval"),
-		     hidden(-name => "dlid",
-			    -default => $dl->{gid}),
-		     end_form()),
-	          td(p($progress));
-	    print end_Tr();
-	}
-	print end_table();
+            my ($basedir) = ($dl->{files}[0]{path} =~ m#^(.*)/#);
+            print td(dl(dt({ -onClick => "toggle(".$dl->{gid}.")" }, $basedir),
+                        map({ +dd({-id => $dl->{gid}}, $_->{path}) }
+                            @{$dl->{files}})));
+            my $progress;
+            if ($dl->{totalLength}) {
+                $progress = sprintf(
+                    "%.2f%% (%s/%s)",
+                    100*$dl->{completedLength} / $dl->{totalLength},
+                    format_bytes($dl->{completedLength}),
+                    format_bytes($dl->{totalLength}));
+            } else {
+                $progress = "n/a";
+            }
+            print td(p($dl->{status}),
+                     start_form(),
+                     popup_menu(-name => "dloptname", -values => [keys %ariadlopts],
+                                -labels => \%ariadlopts),
+                     textfield(-name => "dloptval"),
+                     hidden(-name => "dlid",
+                            -default => $dl->{gid}),
+                     end_form()),
+                  td(p($progress));
+            print end_Tr();
+        }
+        print end_table();
     } else {
-	print p("No downloads");
+        print p("No downloads");
     }
 }
-
 
 open ARIAURL, dirname($ENV{"SCRIPT_FILENAME"}) . "/ariaurl.txt";
 my $ariaurl = <ARIAURL>;
@@ -79,9 +78,9 @@ my $ariactl = Frontier::Client->new(url => $ariaurl);
 binmode STDOUT, ':utf8';
 print header(),
     start_html(-title => "Aria Control",
-	       -style => { -src => "/style.css" },
-	       -script => { -src => "/behaviour.js" },
-	       -encoding => "UTF-8");
+               -style => { -src => "/style.css" },
+               -script => { -src => "/behaviour.js" },
+               -encoding => "UTF-8");
 
 my $v;
 eval {
@@ -97,11 +96,11 @@ if ($@) {
 print h1("Aria Control"),
     start_form(),
     fieldset(legend("Add downloads"),
-	     "URL to add to the queue of Aria: ",
-	     textfield(-name => "url"), br(),
-	     "Output directory: ",
-	     textfield(-name => "dir", -value => "/storage"), br(),
-	     submit()),
+             "URL to add to the queue of Aria: ",
+             textfield(-name => "url"), br(),
+             "Output directory: ",
+             textfield(-name => "dir", -value => "/storage"), br(),
+             submit()),
     end_form();
 
 print
@@ -109,7 +108,7 @@ print
     fieldset(legend("Aria2c options"),
              "Option: ",
              popup_menu(-name => "optname", -values => [keys %ariaopts],
-			-labels => \%ariaopts), br(),
+                        -labels => \%ariaopts), br(),
              "Value: ",
              textfield(-name => "optval"), br(),
              submit()),
@@ -127,7 +126,7 @@ if (my $optname = param("optname") and
     my $optval = param("optval")) {
     print p("Setting $optname to $optval...");
     my $resp = $ariactl->call("aria2.changeGlobalOption",
-			      {$optname => $optval});
+                              {$optname => $optval});
     print p("Aria2c said $resp.");
 }
 
@@ -136,7 +135,7 @@ if (my $dlid = param("dlid") and
     my $optval = param("dloptval")) {
     print p("Setting $optname to $optval for download $dlid...");
     my $resp = $ariactl->call("aria2.changeOption",
-			      $ariactl->string($dlid), {$optname => $optval});
+                              $ariactl->string($dlid), {$optname => $optval});
     print p("Aria2c said $resp.");
 }
 
@@ -161,4 +160,3 @@ print p("Options: "),
     table(map { Tr(td[$_, $options->{$_}]) if !/passwd/; } keys %$options);
 
 print end_html();
-
